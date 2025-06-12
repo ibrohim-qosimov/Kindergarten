7956a445f9cae6784f89111109df4a951d6bbc8d
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2025-06-12 14:50:27 +0500

    get function

--- Kindergarten.Application/UseCases/ChildrenServices/Queries/GetChildByIdQuery.cs
+++ Kindergarten.Application/UseCases/ChildrenServices/Queries/GetChildByIdQuery.cs
@@ -1,4 +1,8 @@
-﻿namespace Kindergarten.Application.UseCases.ChildrenServices.Queries;
-public class GetChildByIdQuery
+﻿using Kindergarten.Domain.Entities;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.ChildrenServices.Queries;
+public class GetChildByIdQuery : IRequest<Child>
 {
+    public int Id { get; set; }
 }
--- Kindergarten.Application/UseCases/ChildrenServices/Queries/GetChildByIdQueryHandler.cs
+++ Kindergarten.Application/UseCases/ChildrenServices/Queries/GetChildByIdQueryHandler.cs
@@ -1,4 +1,25 @@
-﻿namespace Kindergarten.Application.UseCases.ChildrenServices.Queries;
-public class GetChildByIdQueryHandler
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.ChildrenServices.Queries;
+public class GetChildByIdQueryHandler : IRequestHandler<GetChildByIdQuery, Child>
 {
+    private readonly IApplicationDbContext _dbContext;
+
+    public GetChildByIdQueryHandler(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Child> Handle(GetChildByIdQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _dbContext
+                .Children
+                    .FirstOrDefaultAsync(c => c.Id == request.Id)
+                        ?? throw new Exception();
+
+        return response;
+    }
 }
